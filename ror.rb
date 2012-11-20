5010d7a40cb4048174b436f62585c1617574f041
Miha Rebernik <miha@rebernik.info>
2012-11-20 00:00:42 -0800

    Adding rack-cache into the game.

--- ror.rb
+++ ror.rb
@@ -1,15 +1,20 @@
-require 'sinatra'
+require "rubygems"
+require "bundler"
+Bundler.require :default, (ENV["RACK_ENV"] || "development").to_sym
 
 get '/' do
+  cache_control :public, max_age: 86400
   haml :index
 end
 
 get '/download/' do
+  cache_control :public, max_age: 86400
   @current = :download
   haml :download
 end
 
 get '/screencasti/' do
+  cache_control :public, max_age: 86400
   @current = :screencasti
   haml :screencasti
 end
